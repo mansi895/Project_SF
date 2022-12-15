b5e769324b3640ef5e122a071219f94c1efc06b4
mansi895 <109337080+mansi895@users.noreply.github.com>
2022-12-15 17:27:51 +0530

    Update sf.py

--- sf.py
+++ sf.py
@@ -14,7 +14,7 @@ for data1 in data_bitcoin:
     if data1 not in data_captured:
         data_captured.append(data1)
         api_data=requests.get(f'https://api.senticrypt.com/v1/history/{data1}')
-        df1=pd.DataFrame(columns=['data'])
+        df1=pd.DataFrame(columns=['p1'])
         df1["data"]=json.loads(api_data.text)
         df=pd.concat([df,df1])
     else:
@@ -36,4 +36,4 @@ success,nchunks,nrows ,_ =write_pandas(connect1,df,'S1')
 
 my_cur=connect1.cursor()
 my_cur.execute('select * from S1')
-print(my_cur.fetchone())
+print(my_cur.fetchall())
